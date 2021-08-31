52d5f249c3aefc2ee85140f61a98c771503a2240
Vadim <vadimbaranivsky83@gmail.com>
2021-08-31 17:29:12 +0300

    fix

--- src/features/Standalone components/TextareaForm.js
+++ src/features/Standalone components/TextareaForm.js
@@ -7,18 +7,18 @@ function TextareaForm({onSend, autosaveAdress = false}) {
     let textareaRef = React.useRef(null)
 
         // saves textarea in case of page reload 
-        React.useEffect(() => {
-            autosaveAdress && ( async () => {
-                const response = await axios.get(serverPath + `/autosave/${autosaveAdress}`)
-                textareaRef.current.value =response.data.value
-            })()
-        },[])
+        // React.useEffect(() => {
+        //     autosaveAdress && ( async () => {
+        //         const response = await axios.get(serverPath + `/autosave/${autosaveAdress}`)
+        //         textareaRef.current.value =response.data.value
+        //     })()
+        // },[])
     
-        React.useEffect(() => setInterval( () => {
-            textareaRef.current && autosaveAdress &&
-            axios.put(serverPath + `/autosave/${autosaveAdress}`, {id:`${autosaveAdress}`, value: `${textareaRef.current.value}` })
-        } , 5000
-        ),[])
+        // React.useEffect(() => setInterval( () => {
+        //     textareaRef.current && autosaveAdress &&
+        //     axios.put(serverPath + `/autosave/${autosaveAdress}`, {id:`${autosaveAdress}`, value: `${textareaRef.current.value}` })
+        // } , 5000
+        // ),[])
 
     //changes size according to text
     const changeTextareaSize = async () => {
--- src/utils/subscribePushNotifications.js
+++ src/utils/subscribePushNotifications.js
@@ -4,21 +4,21 @@ import serverPath from "../index";
 
 
 export default async function subscribePushNotifications(id) {
-  if ('serviceWorker' in navigator) {
-    const response = await axios.get(serverPath + '/publicVapidKey')
-    const publicVapidKey = response.data
-    const register = await navigator.serviceWorker.register(`${process.env.PUBLIC_URL}/serviceWorker.js`)
+  // if ('serviceWorker' in navigator) {
+  //   const response = await axios.get(serverPath + '/publicVapidKey')
+  //   const publicVapidKey = response.data
+  //   const register = await navigator.serviceWorker.register(`${process.env.PUBLIC_URL}/serviceWorker.js`)
 
 
-      await register.pushManager.subscribe({
-        userVisibleOnly: true,
-        applicationServerKey: urlBase64ToUint8Array(publicVapidKey)
-      }).then(subscription => {
-        axios.post(serverPath + '/subscription', {subscription, id})
-      })
+  //     await register.pushManager.subscribe({
+  //       userVisibleOnly: true,
+  //       applicationServerKey: urlBase64ToUint8Array(publicVapidKey)
+  //     }).then(subscription => {
+  //       axios.post(serverPath + '/subscription', {subscription, id})
+  //     })
 
       
-  }
+  // }
   
     
 }
