bd7b15e6ace01decb0aa3e7575f52e5880e02170
Vadim <vadimbaranivsky83@gmail.com>
2021-08-31 22:31:12 +0300

    fix

--- src/features/CreateLetterModal/CreateLetterForm/API.js
+++ src/features/CreateLetterModal/CreateLetterForm/API.js
@@ -9,11 +9,11 @@ export const fetchData = async () => {
 }
 
 export const postData = async (data) => {
-    const testDate = new Date()
-    testDate.setSeconds(parseInt(testDate.getSeconds() + 20))
-    data.flightControl.time.from = testDate.toISOString()
-    data.flightControl.time.to = testDate.toISOString()
-    console.log(serverPath + '/scheduledLetters')
+    // const testDate = new Date()
+    // testDate.setSeconds(parseInt(testDate.getSeconds() + 20))
+    // data.flightControl.time.from = testDate.toISOString()
+    // data.flightControl.time.to = testDate.toISOString()
+    // console.log(serverPath + '/scheduledLetters')
     const response = await axios.post(serverPath + '/scheduledLetters', data);
     return response.data
 }
