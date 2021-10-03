cf86ef079e9d21402d452de8ddafcceb98cd451b
Vadim <vadimbaranivsky83@gmail.com>
2021-10-03 04:06:46 +0300

    change api path /sendLetter to /letters/sendLetter

--- src/features/CreateLetterModal/CreateLetterForm/API.js
+++ src/features/CreateLetterModal/CreateLetterForm/API.js
@@ -9,12 +9,11 @@ export const fetchData = async () => {
 }
 
 export const postData = async (data) => {
-    // const testDate = new Date()
-    // testDate.setSeconds(parseInt(testDate.getSeconds() + 20))
-    // data.flightControl.time.from = testDate.toISOString()
-    // data.flightControl.time.to = testDate.toISOString()
-    // console.log(serverPath + '/scheduledLetters')
-    const response = await axios.post(serverPath + '/sendLetter', data);
+    const testDate = new Date()
+    testDate.setSeconds(parseInt(testDate.getSeconds() + 30))
+    data.flightControl.time.from = testDate.toISOString()
+    data.flightControl.time.to = testDate.toISOString()
+    const response = await axios.post(serverPath + '/letters/sendLetter', data);
     console.log(response)
     return response.data
 }
