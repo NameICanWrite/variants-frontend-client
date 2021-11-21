1c9d58659cb4ba504bfa33dca806bf984e8362fd
Vadim <vadimbaranivsky83@gmail.com>
2021-11-21 20:45:54 +0300

    gads tag

--- src/redux/inbox.slice.js
+++ src/redux/inbox.slice.js
@@ -1,4 +1,4 @@
-import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import { fetchData } from '../components/Inbox/API'
 
 //** important:  ALWAYS REINITIALIZE ARRAY 
